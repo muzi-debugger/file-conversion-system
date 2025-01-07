e7594fbf7ca391a42577e848f4f60f425804c0e9
muzi-debugger <mmzolodev@gmail.com>
2025-01-07 16:53:08 +0200

    fix eslint.config issue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,116 +1,77 @@
-// @ts-check
-
 import globals from 'globals';
 import prettier from 'eslint-plugin-prettier/recommended';
 import tseslint from 'typescript-eslint';
 import eslint from '@eslint/js';
 import react from 'eslint-plugin-react/configs/recommended.js';
 import cypress from 'eslint-plugin-cypress/flat';
+
 // jhipster-needle-eslint-add-import - JHipster will add additional import here
 
-export default tseslint.config(
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-      },
+export default {
+  languageOptions: {
+    globals: {
+      ...globals.node,
+    },
+    parserOptions: {
+      ecmaVersion: 2021,
     },
   },
-  { ignores: ['src/main/docker/'] },
-  { ignores: ['target/classes/static/', 'target/'] },
-  eslint.configs.recommended,
-  {
-    files: ['**/*.{js,cjs,mjs}'],
-    rules: {
-      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    },
+  extends: [
+    'eslint:recommended', // Use string 'eslint:recommended' for ESLint's recommended rules
+    'plugin:react/recommended',
+  ],
+  rules: {
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'constructor-super': 'off', // Disable constructor-super rule
   },
-  {
-    files: ['src/main/webapp/**/*.{ts,tsx}'],
-    extends: [...tseslint.configs.recommendedTypeChecked, react],
-    settings: {
-      react: {
-        version: 'detect',
+  overrides: [
+    {
+      files: ['**/*.{js,cjs,mjs}'],
+      rules: {
+        'constructor-super': 'off', // Disable constructor-super rule
       },
     },
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-      },
-      parserOptions: {
-        project: ['./tsconfig.json', './tsconfig.test.json'],
-      },
-    },
-    rules: {
-      '@typescript-eslint/member-ordering': [
-        'error',
-        {
-          default: ['static-field', 'instance-field', 'constructor', 'static-method', 'instance-method'],
+    {
+      files: ['src/main/webapp/**/*.{ts,tsx}'],
+      extends: [...tseslint.configs.recommendedTypeChecked, react],
+      settings: {
+        react: {
+          version: 'detect',
         },
-      ],
-      '@typescript-eslint/no-unused-vars': 'off',
-      '@typescript-eslint/explicit-member-accessibility': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unsafe-argument': 'off',
-      '@typescript-eslint/no-unsafe-return': 'off',
-      '@typescript-eslint/no-unsafe-member-access': 'off',
-      '@typescript-eslint/no-unsafe-call': 'off',
-      '@typescript-eslint/no-unsafe-assignment': 'off',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/restrict-template-expressions': 'off',
-      '@typescript-eslint/restrict-plus-operands': 'off',
-      '@typescript-eslint/no-floating-promises': 'off',
-      '@typescript-eslint/interface-name-prefix': 'off',
-      '@typescript-eslint/no-empty-function': 'off',
-      '@typescript-eslint/unbound-method': 'off',
-      '@typescript-eslint/array-type': 'off',
-      '@typescript-eslint/no-misused-promises': 'off',
-      '@typescript-eslint/no-shadow': 'error',
-      'spaced-comment': ['warn', 'always'],
-      'guard-for-in': 'error',
-      'no-labels': 'error',
-      'no-caller': 'error',
-      'no-bitwise': 'error',
-      'no-console': ['error', { allow: ['warn', 'error'] }],
-      'no-new-wrappers': 'error',
-      'no-eval': 'error',
-      'no-new': 'error',
-      'no-var': 'error',
-      radix: 'error',
-      eqeqeq: ['error', 'always', { null: 'ignore' }],
-      'prefer-const': 'error',
-      'object-shorthand': ['error', 'always', { avoidExplicitReturnArrows: true }],
-      'default-case': 'error',
-      complexity: ['warn', 40],
-      'no-invalid-this': 'off',
-      'react/prop-types': 'off',
-      'react/display-name': 'off',
-    },
-  },
-  {
-    files: ['src/main/webapp/**/*.spec.ts'],
-    rules: {
-      '@typescript-eslint/no-empty-function': 'off',
-    },
-  },
-  {
-    files: ['src/test/javascript/cypress/**/*.ts'],
-    extends: [...tseslint.configs.recommendedTypeChecked, cypress.configs.recommended],
-    languageOptions: {
-      parserOptions: {
-        project: ['./src/test/javascript/cypress/tsconfig.json'],
+      },
+      languageOptions: {
+        globals: {
+          ...globals.browser,
+        },
+        parserOptions: {
+          project: ['./tsconfig.json', './tsconfig.test.json'],
+        },
+      },
+      rules: {
+        // Add specific TypeScript rules here
       },
     },
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unsafe-argument': 'off',
-      '@typescript-eslint/no-unsafe-assignment': 'off',
-      '@typescript-eslint/no-unsafe-call': 'off',
-      '@typescript-eslint/no-unsafe-member-access': 'off',
-      '@typescript-eslint/unbound-method': 'off',
+    {
+      files: ['src/main/webapp/**/*.spec.ts'],
+      rules: {
+        '@typescript-eslint/no-empty-function': 'off',
+      },
     },
-  },
+    {
+      files: ['src/test/javascript/cypress/**/*.ts'],
+      extends: [...tseslint.configs.recommendedTypeChecked, cypress.configs.recommended],
+      languageOptions: {
+        parserOptions: {
+          project: ['./src/test/javascript/cypress/tsconfig.json'],
+        },
+      },
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unsafe-argument': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+      },
+    },
+  ],
   // jhipster-needle-eslint-add-config - JHipster will add additional config here
-  prettier,
-);
+  ...prettier,
+};
